7b72a742945b856c819e406e535acc38538f942c
nandana-sureshh <nandanasuresh2468@gmail.com>
2026-06-26 15:23:54 +0530

    chore(infra): remove 7 unused EC2-era modules (alb, asg, bastion, cloudfront, iam, launch-templates, sns)

--- terraform/modules/asg/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "frontend_asg_name" {
-  description = "Frontend Auto Scaling Group name"
-  value       = aws_autoscaling_group.frontend.name
-}
-
-output "backend_asg_name" {
-  description = "Backend Auto Scaling Group name"
-  value       = aws_autoscaling_group.backend.name
-}
